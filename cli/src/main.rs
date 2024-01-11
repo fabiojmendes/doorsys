6ff5b337e1f42cfde1bdd0bc9eff0b20b387b7f6
Fabio Mendes <fabiojmendes@gmail.com>
2024-01-11 17:28:59 -0500

    Removing hardcoded addresses

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,38 +1,67 @@
-use std::time::Duration;
+use std::{
+    env,
+    error::Error,
+    thread,
+    time::{Duration, SystemTime},
+};
 
 use bincode::config::Configuration;
 use chrono::{DateTime, Utc};
-use doorsys_protocol::Audit;
+use doorsys_protocol::{Audit, CodeType};
 use rumqttc::{Client, Event, MqttOptions, Packet, QoS};
 
 const CONFIG: Configuration = bincode::config::standard();
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Starting doorsys-cli");
 
-    let mut mqtt_opts = MqttOptions::new("doorsys-cli", "rpi.home", 1883);
-    mqtt_opts
-        .set_keep_alive(Duration::from_secs(5))
-        .set_credentials("esp", "aurora")
-        .set_clean_session(false);
+    let mqtt_url = env::var("MQTT_URL")?;
+    let mqtt_opts = MqttOptions::parse_url(mqtt_url)?;
 
     let (mut client, mut connection) = Client::new(mqtt_opts, 10);
-    client.subscribe("doorsys/user", QoS::AtLeastOnce).unwrap();
 
+    let audit = Audit {
+        timestamp: SystemTime::now(),
+        code_type: CodeType::Pin,
+        code: 1234,
+        success: true,
+    };
+    let payload = bincode::encode_to_vec(audit, CONFIG).unwrap();
+    client
+        .publish("doorsys/audit", QoS::AtLeastOnce, false, &*payload)
+        .unwrap();
+    client
+        .publish("doorsys/audit", QoS::AtLeastOnce, false, &*payload)
+        .unwrap();
+    client
+        .publish("doorsys/audit", QoS::AtLeastOnce, false, &*payload)
+        .unwrap();
     // Iterate to poll the eventloop for connection progress
     for (i, notification) in connection.iter().enumerate() {
-        println!("[{i}] Notification = {:?}", &notification);
-        if let Ok(Event::Incoming(Packet::Publish(p))) = notification {
-            println!(
-                "topic: {}, qos: {:?}, data: {:?}",
-                p.topic, p.qos, p.payload
-            );
-            if let Ok((audit, count)) = bincode::decode_from_slice::<Audit, _>(&p.payload, CONFIG) {
-                println!("Audit: {:?} {}", audit, count);
-                println!("systime: {:?}", &audit.timestamp);
-                let date: DateTime<Utc> = audit.timestamp.into();
-                println!("datetime: {}", date);
+        match notification {
+            Ok(Event::Incoming(Packet::Publish(p))) => {
+                println!(
+                    "topic: {}, qos: {:?}, data: {:?}",
+                    p.topic, p.qos, p.payload
+                );
+                if let Ok((audit, count)) =
+                    bincode::decode_from_slice::<Audit, _>(&p.payload, CONFIG)
+                {
+                    println!("Audit: {:?} {}", audit, count);
+                    println!("systime: {:?}", &audit.timestamp);
+                    let date: DateTime<Utc> = audit.timestamp.into();
+                    println!("datetime: {}", date);
+                }
+            }
+            Err(rumqttc::ConnectionError::Io(e)) => {
+                println!("Connection refused {:?}", e);
+                thread::sleep(Duration::from_secs(5));
+            }
+            _ => {
+                println!("[{i}] Notification = {:?}", &notification);
             }
         }
     }
+
+    Ok(())
 }
