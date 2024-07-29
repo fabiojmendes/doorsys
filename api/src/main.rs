18134c6ea73f75ea1eefeae3f19a96406ade57cd
Fabio Mendes <fabiojmendes@gmail.com>
2024-07-29 15:23:59 -0400

    Revert "Adding custom log formatter"
    This reverts commit 79ae217c2f1ac6d11679b9095a5950e1054b3f6d.

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,14 +1,19 @@
+use std::env;
+
 mod domain;
 mod http;
-mod logging;
 mod mqtt;
 
 use sqlx::postgres::PgPoolOptions;
-use std::env;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    logging::init();
+    tracing_subscriber::fmt()
+        .without_time()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
     tracing::info!("Starting Server");
 
     let pool = PgPoolOptions::new()
